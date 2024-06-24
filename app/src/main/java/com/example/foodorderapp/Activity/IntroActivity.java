dfcfea2b31f00b87297fd33ae873d0861431701c
srpraju <srpraju1554@gmail.com>
2024-06-25 01:30:07 +0530

    Project Setup

---
+++ app/src/main/java/com/example/foodorderapp/Activity/IntroActivity.java
@@ -0,0 +1,17 @@
+package com.example.foodorderapp.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodorderapp.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+    }
+}
