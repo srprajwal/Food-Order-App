74abd5acc592df9c7483fc6b3ee351b77a75f02b
srpraju <srpraju1554@gmail.com>
2024-06-25 18:18:08 +0530

    LoginActivity Ui complete

--- app/src/main/java/com/example/foodorderapp/Activity/IntroActivity.java
+++ app/src/main/java/com/example/foodorderapp/Activity/IntroActivity.java
@@ -1,17 +1,42 @@
 package com.example.foodorderapp.Activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.foodorderapp.R;
+import com.example.foodorderapp.databinding.ActivityIntroBinding;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity extends BaseActivity {
+
+    ActivityIntroBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+
+        setVariable();
+        getWindow().setStatusBarColor(Color.parseColor("#FFE4B5"));
+    }
+
+    private void setVariable() {
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        binding.signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/foodorderapp/Activity/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.foodorderapp.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.foodorderapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
