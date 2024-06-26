5571b14de5d37d1eed2379bf42ae450dcb5d0e0e
srpraju <srpraju1554@gmail.com>
2024-06-27 01:04:45 +0530

    ListFoodsActivity Complect

---
+++ app/src/main/java/com/example/foodorderapp/Activity/ListFoodsActivity.java
@@ -0,0 +1,89 @@
+package com.example.foodorderapp.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.foodorderapp.Adapter.FoodListAdapter;
+import com.example.foodorderapp.Domain.Foods;
+import com.example.foodorderapp.R;
+import com.example.foodorderapp.databinding.ActivityListFoodsBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class ListFoodsActivity extends BaseActivity {
+    ActivityListFoodsBinding binding;
+    private RecyclerView.Adapter adapterListFood;
+    private int categoryId;
+    private String categoryName;
+    private  String searchTxt;
+    private boolean isSearch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityListFoodsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getIntentExtra();
+        initList();
+    }
+
+    private void initList() {
+        DatabaseReference myRef = database.getReference("Foods");
+        binding.progressBar.setVisibility(View.VISIBLE);
+
+        ArrayList<Foods> list = new ArrayList<>();
+
+        Query query;
+        if(isSearch){
+            query = myRef.orderByChild("Title").startAt(searchTxt).endAt(searchTxt+'\uf8ff');
+        }else{
+            query = myRef.orderByChild("CategoryId").equalTo(categoryId);
+        }
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for(DataSnapshot issue: snapshot.getChildren()){
+                        list.add(issue.getValue(Foods.class));
+                    }
+                    if(!list.isEmpty()){
+                        binding.foodListView.setLayoutManager(new GridLayoutManager(ListFoodsActivity.this,2));
+                        adapterListFood = new FoodListAdapter(list);
+                        binding.foodListView.setAdapter(adapterListFood);
+                    }
+                    binding.progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getIntentExtra() {
+        categoryId = getIntent().getIntExtra("CategoryId",0);
+        categoryName = getIntent().getStringExtra("CategoryName");
+        searchTxt= getIntent().getStringExtra("text");
+        isSearch = getIntent().getBooleanExtra("isSearch", false);
+
+        binding.titleTxt.setText(categoryName);
+        binding.backBtn.setOnClickListener(v -> finish());
+    }
+}
--- app/src/main/java/com/example/foodorderapp/Adapter/CategoryAdapter.java
+++ app/src/main/java/com/example/foodorderapp/Adapter/CategoryAdapter.java
@@ -1,6 +1,7 @@
 package com.example.foodorderapp.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.example.foodorderapp.Activity.ListFoodsActivity;
 import com.example.foodorderapp.Domain.Category;
 import com.example.foodorderapp.Domain.Foods;
 import com.example.foodorderapp.R;
@@ -80,6 +82,13 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.viewho
         Glide.with(context)
                 .load(drawableResourceId)
                 .into(holder.pic);
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, ListFoodsActivity.class);
+            intent.putExtra("CategoryId",items.get(position).getId());
+            intent.putExtra("CategoryName",items.get(position).getName());
+            context.startActivity(intent);
+        });
     }
 
     @Override
